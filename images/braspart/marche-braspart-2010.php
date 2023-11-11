d16ca6576b35a9fd67711eb90d9ad9ec715550aa
lcv29 <150371914+lcv29@users.noreply.github.com>
2023-11-11 18:06:28 +0100

    Add files via upload

---
+++ images/braspart/marche-braspart-2010.php
@@ -0,0 +1,203 @@
+<!doctype html>
+	<?php
+	session_start();
+	?>
+
+	<html lang="fr">
+  <head>
+    <title>Diaporama</title>
+	<link rel="stylesheet" href="diaporama.css">
+	<h3 style="text-align:center;"> Marche Braspart 2010</h3> 
+
+  </head>
+  <body>
+    <div class="slide-container">
+      <div class="custom-slider fade">
+        <div class="slide-index">1 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_01.jpg">
+        <div class="slide-text">Diapo1.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">2 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_02.jpg">
+        <div class="slide-text">Diapo2. </div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">3 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_03.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">4 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_04.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">5 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_05.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">6 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_06.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">7 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_07.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">8 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_08.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">9 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_09.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">10 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_10.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">11 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_11.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">12 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_12.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">13 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_13.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">14 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_14.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">15 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_15.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">16 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_16.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">17 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_17.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">18 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_18.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">19 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_19.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">20 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_20.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">21 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_21.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">22 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_22.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">23 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_23.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">24 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_24.jpg">
+        <div class="slide-text">Diapo24.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">25	/ 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_25.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">26 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_26.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">27 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_27.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">28 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_28.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">29 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_29.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">30 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_30.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">31 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_31.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">32 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_32.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">33 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_33.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">34 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_34.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+      <div class="custom-slider fade">
+        <div class="slide-index">35 / 35</div>
+        <img class="slide-img" src="https://lcv.alwaysdata.net/images/marche-braspart-2010/braspart_35.jpg">
+        <div class="slide-text">Diapo3.</div>
+      </div>
+	  
+      <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+      <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    </div>
+    <br>
+    <div class="slide-dot">
+      <span class="dot" onclick="currentSlide(1)"></span> 
+      <span class="dot" onclick="currentSlide(2)"></span> 
+      <span class="dot" onclick="currentSlide(3)"></span> 
+    </div>
+	
+	<script src="diaporama.js"></script>
+  </body>
+</html>
